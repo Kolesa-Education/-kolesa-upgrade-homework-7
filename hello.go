854e830ee66e57a43536d8715c0438c3648c53b2
Zharkyn <resar3ct@gmail.com>
2022-10-13 15:32:09 +0600

    выполняю задание 7

---
+++ hello.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"regexp"
+	"runtime"
+)
+
+func printStats(mem runtime.MemStats) {
+	runtime.ReadMemStats(&mem)
+	fmt.Println("-----")
+	fmt.Println("mem.Alloc:", mem.Alloc)
+	fmt.Println("mem.TotalAlloc:", mem.TotalAlloc)
+	fmt.Println("mem.HeapAlloc:", mem.HeapAlloc)
+	fmt.Println("mem.NumGC:", mem.NumGC)
+	fmt.Println("-----")
+}
+
+func main() {
+	var mem runtime.MemStats
+	printStats(mem)
+
+	var a, b int
+	var c string
+	match, _ := regexp.MatchString("^[a-zA-Z0-9]$", c)
+	fmt.Scan(&a, &c, &b)
+	if match {
+		fmt.Println("не правильный оператор")
+	} else {
+		switch c {
+		case "+":
+			fmt.Println(a + b)
+		case "-":
+			fmt.Println(a - b)
+		case "*":
+			fmt.Println(a * b)
+		case "/":
+			fmt.Println(a / b)
+		case "%":
+			fmt.Println(a % b)
+		case "**":
+			fmt.Println(math.Pow(float64(a), float64(b)))
+		}
+	}
+	printStats(mem)
+}
