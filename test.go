6777f5126b53a162f26f384e995fb9e32011e542
Dilmurat Zairakhunov <dlol96@mail.ru>
2022-10-14 20:22:29 +0600

    поправил

--- test.go
+++ test.go
@@ -4,33 +4,39 @@ import (
 	"errors"
 	"fmt"
 	"math"
+	"reflect"
 )
 
 func main() {
-	var a float64
+	var a int
 	var operation string
-	var b float64
+	var b int
 	fmt.Scan(&a, &operation, &b)
 	calc(a, operation, b)
+
 }
-func calc(a float64, operation string, b float64) {
-	switch operation {
-	case string('+'):
-		fmt.Println(a + b)
-	case string('-'):
-		fmt.Println(a - b)
-	case string('*'):
-		fmt.Println(a * b)
-	case string('/'):
-		if b == 0 {
-			var ErrZeroDivisionAttempt = errors.New("divide by zero is not allowed")
-			fmt.Println(ErrZeroDivisionAttempt)
-		} else {
-			fmt.Println(a / b)
+func calc(a int, operation string, b int) {
+	if reflect.TypeOf(a).Kind() != reflect.Int || reflect.TypeOf(operation).Kind() != reflect.String || reflect.TypeOf(b).Kind() != reflect.Int {
+		fmt.Println("Err input")
+	} else {
+		switch operation {
+		case string('+'):
+			fmt.Println(a + b)
+		case string('-'):
+			fmt.Println(a - b)
+		case string('*'):
+			fmt.Println(a * b)
+		case string('/'):
+			if b == 0 {
+				var ErrZeroDivisionAttempt = errors.New("divide by zero is not allowed")
+				fmt.Println(ErrZeroDivisionAttempt)
+			} else {
+				fmt.Println(a / b)
+			}
+		case string('^'):
+			fmt.Println(math.Pow(float64(a), float64(b)))
+		default:
+			fmt.Println("unknown operator: " + operation)
 		}
-	case string('^'):
-		fmt.Println(math.Pow(a, b))
-	default:
-		fmt.Println("unknown operator: " + operation)
 	}
 }
